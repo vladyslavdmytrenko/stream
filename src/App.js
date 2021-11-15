7184ee6a937603aefa3bcd7a8f984639349bc2d4
vladyslavdmytrenko <vladyslavdmytrenko96@gmail.com>
2021-11-15 17:32:19 +0200

    add provider and store for app component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import StreamCreate from './components/streams/StreamCreate';
 import StreamDelete from './components/streams/StreamDelete';
@@ -6,26 +7,29 @@ import StreamEdit from './components/streams/StreamEdit';
 import StreamList from './components/streams/StreamList';
 import StreamShow from './components/streams/StreamShow';
 import Header from './components/Header';
+import store from './redux/store';
 
 import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Header className="App-header" />
-        <section>
-          <Switch>
-            <Route path="/" exact component={StreamList} />
-            <Route path="/streams/new" exact component={StreamCreate} />
-            <Route path="/streams/edit" exact component={StreamEdit} />
-            <Route path="/streams/delete" exact component={StreamDelete} />
-            <Route path="/streams/show" exact component={StreamShow} />
-            <Redirect to="/" />
-          </Switch>
-        </section>
-      </div>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <div className="App">
+          <Header className="App-header" />
+          <section>
+            <Switch>
+              <Route path="/" exact component={StreamList} />
+              <Route path="/streams/new" exact component={StreamCreate} />
+              <Route path="/streams/edit" exact component={StreamEdit} />
+              <Route path="/streams/delete" exact component={StreamDelete} />
+              <Route path="/streams/show" exact component={StreamShow} />
+              <Redirect to="/" />
+            </Switch>
+          </section>
+        </div>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
